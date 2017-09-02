9caae1ddacb79f348a3107621df91a8fe9327b22
Abhishek <abhishekasdzxc@gmail.com>
2017-09-02 12:06:04 +0530

    Update fmndata.py

--- fmndata.py
+++ fmndata.py
@@ -2,6 +2,7 @@ import numpy as np
 import pickle as p
 from mlxtend.preprocessing import one_hot
 
+#download the dataset from kaggle and place it in the same foleder and run this file.
 #open and read the content of file
 file='fashion-mnist_train.csv'
 with open(file,'r') as f:
@@ -36,10 +37,10 @@ X=npdata[:,1:]
 #making as per the vector notation
 X_test=X.T; Y_test=one_hot(Y.reshape(Y.shape[0],)).T
  #debugging
-# fashion_mnist_data={"X_test":X_test,"X_train":X_train,"Y_test":Y_test,"Y_train":Y_train}
-# p.dump(fashion_mnist_data,open('fmnistdata.d','wb'))
-
-#DATA IS DUMPED WITH THE SAME NAME!!!!!!^
+fashion_mnist_data={"X_test":X_test,"X_train":X_train,"Y_test":Y_test,"Y_train":Y_train}
+p.dump(fashion_mnist_data,open('fmnistdata.d','wb'))
+print("Done!!!! fmnistdata.d is saved")
+exit()
 """
 data=p.load(open('fmnistdata.d','rb'))
 data is a dictionary and can be used to obtain the values
